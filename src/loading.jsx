04fee75ecbf341812c28280af18bcdd0d47ed506
Ahtisham-pd <sa8449006@gmail.com>
2025-09-12 05:15:37 +0500

    fixed loader

--- src/loading.jsx
+++ src/loading.jsx
@@ -2,5 +2,9 @@ import BallsLoading from "./components/Spinner/BallsLoading";
 
 export default function Loading() {
   // Or a custom loading skeleton component
-  return <BallsLoading />
+  return (
+    <div className="h-100 w-100 d-flex align-items-center justify-content-center">
+      <BallsLoading />
+    </div>
+  )
 }
